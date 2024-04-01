a4f5c8f7db0d90b6c4eba4e2325bec5af0a7bccd
acsamendes <acsasantos@discente.ufg.br>
2024-04-01 15:23:21 -0300

    atividade1

---
+++ atividade1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        
+        PessoaFisica pf = new PessoaFisica(scan.nextLine(), scan.nextLine(),scan.nextLine(), scan.nextLine());
+
+        PessoaJuridica pj = new PessoaJuridica(scan.nextLine(), scan.nextLine(), scan.nextLine(), scan.nextLine());
+        
+        System.out.println("\n\n");
+        
+        pf.boletim();
+        System.out.println("\n#######################\n");
+        pj.boletim();
+
+        scan.close();
+    }
+}
